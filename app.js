601de40804fea346ac0db9623f48c79a0c939c25
rafalos <rsulkowski1@gmail.com>
2018-01-22 17:24:04 +0100

    connect database

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require("express"),
-      app = express();
-      
+    app = express(),
+    router = express.Router(),
+    bodyParser = require("body-parser"),
+    mongoose=require("mongoose");
+    
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+mongoose.connect('mongodb://rafalos:rafal1@ds111608.mlab.com:11608/hungr');
 
-app.get("/", function(req,res){
-    res.send("works")
+
+router.get("/", function(req,res){
+    res.json({
+        message: "Hi"
+    })
 })
 
 
 
-
+app.use('/api', router);
 app.listen(3003, "localhost", function(){
     console.log("server has started");
 })
