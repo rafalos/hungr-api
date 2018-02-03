ffe0d1cdd7c56e2276a2674f6dc600dd4a40a5b6
rafalos <rsulkowski1@gmail.com>
2018-02-03 20:53:16 +0100

    set route

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ var express = require("express"),
     router = express.Router(),
     bodyParser = require("body-parser"),
     mongoose=require("mongoose"),
-    Serie = require("./models/serie");  
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    mongoose.connect('mongodb://rafalos:rafalos@ds223268.mlab.com:23268/series7');
+    Serie = require("./models/serie");
 
-
-
-
+app.use(bodyParser.urlencoded({ extended: true })),
+app.use(bodyParser.json()),
+mongoose.connect('mongodb://rafalos:rafalos@ds223268.mlab.com:23268/series7');
 
 router.get("/series", function(req,res){
     Serie.find({}, function(err, foundSeries){
